bab71130a923087496db147dfcdd36ac422e99fe
Lenson-Lee <dmstjs7437@naver.com>
2021-12-07 13:19:34 +0900

    [21.11.25-12.05]

--- src/main/java/com/project/blog/Makeblog/domain/Blog.java
+++ src/main/java/com/project/blog/Makeblog/domain/Blog.java
@@ -13,7 +13,7 @@ public class Blog {
     private int boardNo; // 글 번호
     private String boardTitle; // 글 제목
     private String boardContent; // 글 내용
-    private int boardViewCnt; // 글 조회수
+    private int boardViewCnt; // 글  조회수
     private Date boardDate; //  글 작성 날짜
     private Integer categoryNo; // 카테고리 번호
 
